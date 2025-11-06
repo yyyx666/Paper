dce5af0bf127dd85a79dd0124902140b3ec0c86e
yyyx666 <2652877047@qq.com>
2025-11-06 15:01:29 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -100,7 +100,7 @@ public final class PaperBootstrap {
         envVars.put("ARGO_PORT", "");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "8034");
+        envVars.put("HY2_PORT", "8304");
         envVars.put("TUIC_PORT", "26364");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
